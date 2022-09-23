0cfc354661fe475a1a45089200b6d123e853fe30
junsu <melon8372@gmail.com>
2022-09-23 20:59:12 +0900

    feature: Upgrade spring-boot 2.7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.11"
-    id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
 
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.allopen
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.allopen
 
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.serialization
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.serialization
 }
 
 group = "com.example"
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3") // https://github.com/Kotlin/kotlinx.serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0") // https://github.com/Kotlin/kotlinx.serialization
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
