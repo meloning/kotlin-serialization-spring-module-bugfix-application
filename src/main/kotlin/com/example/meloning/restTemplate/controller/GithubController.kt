4c14526b1314496aa54a3cbfa244fb43327d780b
junsu <melon8372@gmail.com>
2022-12-29 15:52:39 +0900

    feature(RestTemplate): Change return type Nothing to Unit

--- src/main/kotlin/com/example/meloning/restTemplate/controller/GithubController.kt
+++ src/main/kotlin/com/example/meloning/restTemplate/controller/GithubController.kt
@@ -4,6 +4,7 @@ import com.example.meloning.restTemplate.service.GithubService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,13 +15,13 @@ class GithubController(
 ) {
 
     @GetMapping("/v1/users/{github-id}/rest-template")
-    fun getUser(@PathVariable("github-id") githubId: String): ResponseEntity<Nothing> {
+    fun getUser(@PathVariable("github-id") githubId: String): ResponseEntity<Unit> {
         githubService.getUserRestTemplate(githubId)
         return ResponseEntity.noContent().build()
     }
 
     @GetMapping("/v1/users/{github-id}/web-client")
-    fun getUserWebClient(@PathVariable("github-id") githubId: String): ResponseEntity<Nothing> {
+    fun getUserWebClient(@PathVariable("github-id") githubId: String): ResponseEntity<Unit> {
         githubService.getUserWebClient(githubId)
         return ResponseEntity.noContent().build()
     }
